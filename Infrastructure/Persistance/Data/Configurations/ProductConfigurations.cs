b16970fb91c0c15500701cadfb2c83dc8ce6e366
SuperMustafa <mustafahamad116aa@gmail.com>
2025-03-24 23:11:42 +0200

    First_commit

---
+++ Infrastructure/Persistance/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Persistance.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(P => P.ProductBrand)
+                  .WithMany()
+                  .HasForeignKey(P => P.ProductBrandId);
+
+            builder.HasOne(P => P.ProductType)
+                .WithMany()
+                .HasForeignKey(P => P.TypeId);
+            builder.Property(P => P.Price).HasColumnType("decimal(18,3)");
+        }
+    }
+}
